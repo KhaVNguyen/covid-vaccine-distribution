8ecd9b35fb53957c7b4c01639f45ddc86f8fe4a1
minh2804 <minh2804@uw.edu>
2021-02-18 03:09:28 -0800

    Add Proc

--- tom.sql
+++ tom.sql
@@ -1,22 +1,22 @@
-CREATE TABLE tblCUSTOMER_TYPE
-(
+---------------------------------------------------------------------------------------------------
+-- Tables
+---------------------------------------------------------------------------------------------------
+CREATE TABLE tblCUSTOMER_TYPE (
 	CustomerTypeID INT IDENTITY(1,1) PRIMARY KEY,
 	CustomerTypeName VARCHAR(50),
 	CustomerTypeDesc VARCHAR(1000)
 )
 GO
 
-CREATE TABLE tblSUPPLIER
-(
+CREATE TABLE tblSUPPLIER (
 	SupplierID INT IDENTITY(1,1) PRIMARY KEY,
 	SupplierName VARCHAR(50),
 	SupplierDesc VARCHAR(1000),
-	City INT FOREIGN KEY REFERENCES tblCITY(CityID)
+	CityID INT FOREIGN KEY REFERENCES tblCITY(CityID)
 )
 GO
 
-CREATE TABLE tblPRODUCT
-(
+CREATE TABLE tblPRODUCT (
 	ProductID INT IDENTITY(1,1) PRIMARY KEY,
 	ProductName VARCHAR(50),
 	ProductDesc VARCHAR(100),
@@ -24,8 +24,7 @@ CREATE TABLE tblPRODUCT
 )
 GO
 
-CREATE TABLE tblORDER_PRODUCT
-(
+CREATE TABLE tblORDER_PRODUCT (
 	Order_ProductID INT IDENTITY(1,1) PRIMARY KEY,
 	ProductID INT FOREIGN KEY REFERENCES tblProduct(ProductID),
 	OrderID INT FOREIGN KEY REFERENCES tblORDER(OrderID),
@@ -33,16 +32,14 @@ CREATE TABLE tblORDER_PRODUCT
 )
 GO
 
-CREATE TABLE tblDETAIL
-(
+CREATE TABLE tblDETAIL (
 	DetailID INT IDENTITY(1,1) PRIMARY KEY,
 	DetailName VARCHAR(50),
 	DetailDesc VARCHAR(1000)
 )
 GO
 
-CREATE TABLE tblPRODUCT_DETAIL
-(
+CREATE TABLE tblPRODUCT_DETAIL (
 	ProductDetailID INT IDENTITY(1,1) PRIMARY KEY,
 	ProductID INT FOREIGN KEY REFERENCES tblProduct(ProductID),
 	DetailID INT FOREIGN KEY REFERENCES tblDetail(DetailID),
@@ -50,28 +47,132 @@ CREATE TABLE tblPRODUCT_DETAIL
 )
 GO
 
-CREATE PROCEDURE GetCustomerTypeID
+---------------------------------------------------------------------------------------------------
+-- GetID Procedure
+---------------------------------------------------------------------------------------------------
+CREATE PROC GetCustomerTypeID
 @_Name VARCHAR(50),
 @_Out INT OUTPUT
 AS
-IF @_Name IS NULL
-	THROW 50011, '_Name is null', 1;
-SET @_Out = (
-	SELECT CustomerTypeID
-	FROM tblCUSTOMER_TYPE
-	WHERE CustomerTypeName = @_Name
-)
+	IF @_Name IS NULL
+		THROW 50011, '_Name can not be null', 1;
+	SET @_Out = (
+		SELECT CustomerTypeID
+		FROM tblCUSTOMER_TYPE
+		WHERE CustomerTypeName = @_Name
+	)
 GO
 
-CREATE PROCEDURE GetDetailID
+CREATE PROC GetSupplierID
 @_Name VARCHAR(50),
 @_Out INT OUTPUT
 AS
-IF @_Name IS NULL
-	THROW 50011, '_Name is null', 1;
-SET @_Out = (
-	SELECT DetailID
-	FROM tblDETAIL
-	WHERE DetailName = @_Name
-)
+	IF @_Name IS NULL
+		THROW 50012, '_Name can not be null', 1;
+	SET @_Out = (
+		SELECT SupplierID
+		FROM tblSUPPLIER
+		WHERE SupplierName = @_Name
+	)
+GO
+
+CREATE PROC GetProductID
+@_Name VARCHAR(50),
+@_Out INT OUTPUT
+AS
+	IF @_Name IS NULL
+		THROW 50013, '_Name can not be null', 1;
+	SET @_Out = (
+		SELECT ProductID
+		FROM tblPRODUCT
+		WHERE ProductName = @_Name
+	)
+GO
+
+CREATE PROC GetDetailID
+@_Name VARCHAR(50),
+@_Out INT OUTPUT
+AS
+	IF @_Name IS NULL
+		THROW 50014, '_Name can not be null', 1;
+	SET @_Out = (
+		SELECT DetailID
+		FROM tblDETAIL
+		WHERE DetailName = @_Name
+	)
+GO
+
+---------------------------------------------------------------------------------------------------
+-- Insert Proc
+---------------------------------------------------------------------------------------------------
+CREATE PROC AddSupplier
+@SupplierName VARCHAR(50),
+@SupplierDesc VARCHAR(1000),
+@CityName INT
+AS
+	DECLARE @CityID INT
+	EXEC GetCityID
+	@C_Name = @CityName OUTPUT
+
+	IF @CityID IS NULL
+		THROW 50015, 'City not found', 1;
+
+	BEGIN TRAN T1
+		INSERT INTO tblSUPPLIER (SupplierName, SupplierDesc, SupplierID)
+		VALUES (@SupplierName, @SupplierDesc, @CityID)
+
+        IF @@ERROR <> 0
+	ROLLBACK TRAN T1
+        ELSE
+	COMMIT TRAN T1
+GO
+
+CREATE PROC AddProduct
+@ProductName VARCHAR(50),
+@ProductDesc VARCHAR(1000),
+@SupplierName VARCHAR(50)
+AS
+	DECLARE @SupplierID INT
+	EXEC GetSupplierID
+	@_Name = @SupplierName OUTPUT
+
+	IF @SupplierID IS NULL
+		THROW 50016, 'SupplierName not found', 1;
+
+	BEGIN TRAN T1
+		INSERT INTO tblPRODUCT (ProductName, ProductDesc, SupplierID)
+		VALUES (@ProductName, @ProductDesc, @SupplierID)
+
+        IF @@ERROR <> 0
+	ROLLBACK TRAN T1
+        ELSE
+	COMMIT TRAN T1
+GO
+
+CREATE PROC AddProductDetail
+@ProductName VARCHAR(50),
+@DetailName VARCHAR(50),
+@DetailDesc VARCHAR(1000),
+@Value VARCHAR(1000)
+AS
+	DECLARE @ProductID INT
+	EXEC GetProductID
+	@_Name = @ProductName OUTPUT
+
+	IF @ProductID IS NULL
+		THROW 50017, 'ProductName not found', 1;
+
+	BEGIN TRAN T1
+		INSERT INTO tblDETAIL (DetailName, DetailDesc)
+		VALUES (@DetailName, @DetailDesc)
+
+		DECLARE @DetailID = (SCOPE_IDENTITY())
+
+		INSERT INTO tblPRODUCT_DETAIL (ProductID, DetailID, [Value])
+		VALUES (@ProductID, @DetailID, @Value)
+
+        IF @@ERROR <> 0 AND @@TRANCOUNT <> 1
+	ROLLBACK TRAN T1
+        ELSE
+	COMMIT TRAN T1
 GO
