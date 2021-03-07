9e6190f1203045944e67492774c6b47b1b74fdb3
anie0521 <anie0521@uw.edu>
2021-03-06 17:18:19 -0800

    new code main.sql

--- main.sql
+++ main.sql
@@ -1488,7 +1488,7 @@ GROUP BY tblADDRESS.StateID, tblSTATE.StateName
 GO
 
 -- Ranking 1 - 50 orders by states and nums of customer 
-CREATE VIEW vwTop10OrderbyStates
+CREATE VIEW vwTopOrderbyStates
 AS
 SELECT S.StateID, S.StateName, COUNT(O.CustomerID) AS TotalNumsCustomers ,SUM(OP.Quantity) AS TotalProductOrders,
 RANK() OVER (ORDER BY SUM(OP.Quantity) DESC) AS RANK
@@ -1501,16 +1501,17 @@ FROM tblSTATE S
     GROUP BY S.StateID, S.StateName
 GO
 
-SELECT * FROM vwTop10OrderbyStates
 
-CREATE VIEW vwPopularProduct
-SELECT O.OrderID, P.ProductName,SUM(OP.Quantity) AS TotalOrderProduct
+CREATE OR ALTER VIEW vwTheMostTop10PopularProduct
+AS
+SELECT TOP 10 O.OrderID, P.ProductName,SUM(OP.Quantity) AS TotalOrderProduct
 FROM tblORDER O
     JOIN tblORDER_PRODUCT OP ON O.OrderID = OP.OrderID
     JOIN tblPRODUCT P ON OP.ProductID = P.ProductID
     GROUP BY O.OrderID, P.ProductName
     ORDER BY SUM(OP.Quantity) DESC
 GO
+
 
 ---------------------------------------------------------------------------------------------------
 -- Checking Tables
